97891cb7c6fec900e5bf8be2c081c4c27b32d96a
Lucia Sanchez-Mac <lucia_sanchez_@hotmail.com>
2023-01-23 20:22:31 -0300

    Cree componentes header, banner, logo y rrss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsheaderComponent } from './componentsheader/componentsheader.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { RrssComponent } from './components/rrss/rrss.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsheaderComponent,
+    HeaderComponent,
+    BannerComponent,
+    RrssComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
